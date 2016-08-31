05ff0582ae7268d5767d125360db25b63a5e7015
Ate Fokkinga <ate@blueconic.com>
2016-08-31 15:16:34 +0200

    commit test file

--- codeSamples/callback_coffee_0.js
+++ codeSamples/callback_coffee_0.js
@@ -1,38 +1,41 @@
 /* eslint-disable */
 try {
   
-  var money = 10.0;
-  var coffeeTypes = {black: {price:2.5, time:1000}, cappucino: {price:4, time:2000}};
+var money = 10.0;
+var coffeeTypes = {black: {price:2.5, time:1000}, cappucino: {price:4, time:2000}};
 
-  countMoney();
-  getOptions();
-  orderCoffee("black");
-  countMoney();
-  
-  function orderCoffee(type, callback) {
-    print(`Ordering coffee: ${type}`);
-    var coffee = coffeeTypes[type];
-    if (coffee) {
+countMoney();
+getOptions();
+orderCoffee("black");
+countMoney();
 
-      setTimeout(
-        function() {pay(coffee.price);},
-        coffee.time);
-    } else {
-      throw(`coffee '${type}' not available`);
-    }
+function orderCoffee(type, callback) {
+  print(`Ordering coffee: ${type}`);
+  var coffee = coffeeTypes[type];
+  if (coffee) {
+    setTimeout(receiveCoffee, coffee.time, coffee, type);
+  } else {
+    throw(`coffee '${type}' not available`);
   }
+}
 
-  function pay(amount) {money = money - amount;}
-  function countMoney() {print(`Money left ${money}`);}
-  function countMoney() {print(`Money left ${money}`);}
-  function getOptions() {print(`Options are: ${Object.keys(coffeeTypes).join(', ')}`);}
+function receiveCoffee(coffee, type){
+  print(`received coffee ${type}, paying ${coffee.price}`); 
+  pay(coffee.price);
+}
+function pay(amount) {money = money - amount;}
+function countMoney() {print(`Money left ${money}`);}
+function getOptions() {print(`Options are: ${Object.keys(coffeeTypes).join(', ')}`);}
+
 } catch (e) {
   print(e, 'red');
 }
 function print(text, color) {
   console.log(text);
-  if (document) {
+  try{
     var page = document.getElementById("log");
     page.innerHTML = page.innerHTML + `<span style="color:${color || 'black'}">${text}</span>\n`;
+  } catch(e){
+    //silent
   }
 }
--- codeSamples/callback_coffee_1.js
+++ codeSamples/callback_coffee_1.js
@@ -1,29 +1,42 @@
 /* eslint-disable */
+try {
+
 var money = 10.0;
-var coffeeTypes = {black: {price:2.5,time:1000}, cappucino: {price:4, time:2000}};
+var coffeeTypes = {black: {price:2.5, time:1000}, cappucino: {price:4, time:2000}};
 
 countMoney();
 getOptions();
-orderCoffee("black", (error, price) => {
-  if (error) {
-    return console.error(error);    
-  }
-  pay(value.price);
-})
+orderCoffee("black", receiveCoffee);
+orderCoffee("cappucino", receiveCoffee);
 countMoney();
 
 function orderCoffee(type, callback) {
+  print(`Ordering coffee: ${type}`);
   var coffee = coffeeTypes[type];
-  if(coffee){
-    setTimeout(
-      function(){callback(null, coffee.price);}, 
-      coffee.time);
+  if (coffee) {
+    setTimeout(function(){callback(null, coffee, type);},coffee.time);       
   } else {
-    callback(`coffee '${type}' not available`);
+    throw(`coffee '${type}' not available`);
   }
 }
 
+function receiveCoffee(error, coffee, type){
+  print(`received coffee ${type}, paying ${coffee.price}`); 
+  pay(coffee.price);
+}
 function pay(amount) {money = money - amount;}
-function countMoney() {console.log(`Money left ${money}`);}
-function getOptions() {console.log(`Options are: ${Object.keys(coffeeTypes).join(', ')}`);}
+function countMoney() {print(`Money left ${money}`);}
+function getOptions() {print(`Options are: ${Object.keys(coffeeTypes).join(', ')}`);}
 
+} catch (e) {
+  print(e, 'red');
+}
+function print(text, color) {
+  console.log(text);
+  try{
+    var page = document.getElementById("log");
+    page.innerHTML = page.innerHTML + `<span style="color:${color || 'black'}">${text}</span>\n`;
+  } catch(e){
+    //silent
+  }
+}
